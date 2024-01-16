645c82c430a9100bade55efbd45699b7fc307d5c
Esha <maheshwariesha11@gmail.com>
2024-01-16 23:27:57 +0530

    Added notification cron

--- src/index.js
+++ src/index.js
@@ -4,15 +4,20 @@ const bodyParser = require('body-parser');
 const { PORT } = require('./config/serverConfig');
 
 // const { sendBasicEmail } = require('./services/email-service');
+const jobs = require('./utils/job');
+
+const TicketController = require('./controllers/ticket-controller');
 
 const setupAndStartServer = () => {
     const app = express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
+    
+    app.post('/api/v1/tickets', TicketController.create);
 
     app.listen(PORT, () => {
         console.log('Server started at Port:',PORT);
-
+        jobs();
         // sendBasicEmail(
         //     'support@admin.com',
         //     'eshadhoot20@gmail.com',
